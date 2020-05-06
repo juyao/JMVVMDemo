c5e7298da783aa161c45b8424557f40bc56f1867
juyao <juy@weipaitang.com>
2020-05-06 14:35:37 +0800

    格式化

--- app/src/main/java/com/juyao/jmvvmdemo/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/juyao/jmvvmdemo/viewmodel/MainViewModel.kt
@@ -3,16 +3,16 @@ package com.juyao.jmvvmdemo.viewmodel
 import android.util.Log
 import com.juyao.jmvvmdemo.net.Api
 
-class MainViewModel: BaseViewModel() {
-       fun getBanner(){
-           apiRequest(
-                {
-                   Api.getNetService().getBannerData()
-               },
-              {
-                   Log.i("MainViewModel", "onsuccess:$it")
-               }
+class MainViewModel : BaseViewModel() {
+    fun getBanner() {
+        apiRequest(
+            {
+                Api.getNetService().getBannerData()
+            },
+            {
+                Log.i("MainViewModel", "onsuccess:$it")
+            }
 
-           )
+        )
     }
 }
